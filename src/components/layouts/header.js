bf9b6f459e66cb2b6543364cc44eb4ae63183e37
modalsoul0226 <modalsoul0226@gmail.com>
2021-12-12 17:21:05 -0500

    update the style for header

--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -3,19 +3,19 @@ import React from "react"
 const header = {
     width: "100%",
     height: "100%",
-    position: "relative",
+    position: "fixed",
     zIndex: "0"
 }
 
 const headerTitle = {
-    position: "fixed",
+    position: "absolute",
     bottom: "25%"
 }
 
 const Header = () => {
     return (
         <div style={header}>
-            <div className="title" style={headerTitle}>L'Atelier</div>
+            <div className="title" style={headerTitle}>in&nbsp;&nbsp;&nbsp;vention</div>
         </div>
     )
 }
--- src/components/layouts/nav.js
+++ src/components/layouts/nav.js
@@ -5,7 +5,7 @@ const Navigation = () => {
     return (
         <div className={styles.container}>
             <div className={styles.title}>
-                <div className={styles.rouge + " title"}>Rouge</div>
+                <div className={styles.rouge + " title"}>(ter)</div>
             </div>
         </div>
     )
