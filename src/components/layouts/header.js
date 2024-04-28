ea0650cc8879c7aa13ac5b383b1bc344b3d4ad78
modalsoul0226 <modalsoul0226@gmail.com>
2024-04-28 18:27:54 -0400

    turn header into a nav link

--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -1,28 +1,33 @@
-import React from "react"
+import React from 'react'
 
 const header = {
-    width: "100%",
+    width: '100%',
 }
 
 const headerTitle = {
-    width: "100%",
-    position: "sticky",
-    top: "10vh",
-    marginTop: "70vh",
-    lineHeight: "0.4"
+    width: '100%',
+    position: 'sticky',
+    top: '10vh',
+    marginTop: '70vh',
+    lineHeight: '0.4',
 }
 
 const em = {
-    color: "rgba(195,20,20)"
+    color: 'rgba(195,20,20)',
 }
 
 const Header = () => {
     return (
         <div className="hidden md:block" style={header}>
-            <div className="title" style={headerTitle}>
-                in&nbsp;&nbsp;&nbsp;vention<br/>
+            <button
+                onClick={() => (location = '/')}
+                className="title"
+                style={headerTitle}
+            >
+                in&nbsp;&nbsp;&nbsp;vention
+                <br />
                 &nbsp;&nbsp;&nbsp;<span style={em}>(ter)</span>
-            </div>
+            </button>
         </div>
     )
 }
